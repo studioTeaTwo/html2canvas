d84f2a42d42125bd8883fd1560fdb47ad79266e1
T2 <studioTeaTwo@gmail.com>
2020-03-12 16:52:36 +0900

    costom build

---
+++ dist/lib/core/util.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SMALL_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+exports.nodeListToArr = function (nodeList) { return Array.prototype.slice.call(nodeList); };
+//# sourceMappingURL=util.js.map
---
+++ dist/types/core/util.d.ts
@@ -0,0 +1,2 @@
+export declare const SMALL_IMAGE = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+export declare const nodeListToArr: (nodeList: NodeList) => ChildNode[];
